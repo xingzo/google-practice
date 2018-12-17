e23f18e9b5648e3e33162a26c2d6af87d1ceaca6
Kingsley Nyaosi <34792990+xingzo@users.noreply.github.com>
2018-12-18 02:15:54 +0300

    clean up

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
-let guess = ["G","Y","P","P"]
-let answer = ["R", "O","Y", "Y"]
+let guess = ["G","Y","P","P"];
+let answer = ["R", "O","Y", "Y"];
 
 const score = (answer, guess) => {
 
-  let result = []
-  let bCount = 0
-  let wCount = 0
-  let originalAnswer = answer
+  let result = [];
+  let originalAnswer = answer;
 
   //black pegs
   for(let i = 0; i < guess.length; i++){  
     if(guess[i] === answer[i]){
-      bCount++     
+      result.push("B");   
     }
   }
   
   //white pegs
   for(let i = 0; i < guess.length; i++){
     if(answer.indexOf(guess[i]) != -1 && (answer[i]!= guess[i])){
-      wCount++
-      answer[answer.indexOf(guess[i])] = "0"
+      result.push("W");
+      answer[answer.indexOf(guess[i])] = "0";
     }
   }
-  
-  
-  while(bCount > 0){
-    result.push("B")
-    bCount--
-  }
-  
-  while(wCount > 0){
-    result.push("W")
-    wCount--
-    
-  }
 
-  return result
+  return result;
   
 }
 
-console.log(score(answer, guess))
+console.log(score(answer, guess));
